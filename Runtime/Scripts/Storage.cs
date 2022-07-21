61643fa5e1f1cdefb336c03cb67427325d461e6f
Matheus Moura <matheus.moura13@gmail.com>
2022-07-21 01:33:52 -0300

    feat: adiciona PersistentStorage

---
+++ Runtime/Scripts/Storage.cs
@@ -0,0 +1,52 @@
+using HGS.FileControl.Utils;
+using UnityEngine;
+using UnityEngine.Profiling;
+
+namespace HGS.FileControl
+{
+  public abstract class Storage : ScriptableObject
+  {
+    [SerializeField] bool useCrypto = true;
+    [SerializeField] string passPhrase = "your-key-here";
+    [SerializeField] string folder = "data";
+
+    protected string Root => @$"{Application.persistentDataPath}/{folder}/";
+    protected string PassPhrase => passPhrase;
+
+    public virtual bool Exists(string key)
+    {
+      var hash = CriptographyUtility.Hash(key);
+      var path = Root + hash;
+      return FileUtility.Exists(path);
+    }
+
+    public virtual byte[] ReadBytes(string key)
+    {
+      Profiler.BeginSample("Storage Read");
+      var hash = CriptographyUtility.Hash(key);
+
+      if (!Exists(key)) return null;
+
+      var path = Root + hash;
+      var output = FileUtility.Read(path);
+      if (useCrypto) output = CriptographyUtility.Decrypt(output, passPhrase);
+
+      Profiler.EndSample();
+
+      return output;
+    }
+
+    public virtual void WriteBytes(string key, byte[] bytes)
+    {
+      var hash = CriptographyUtility.Hash(key);
+
+      var input = useCrypto
+          ? CriptographyUtility.Encrypt(bytes, passPhrase)
+          : bytes;
+
+      FileUtility.Write(Root, hash, input);
+
+      Debug.Log($"{key} writed in {Root}{hash}");
+    }
+  }
+}
