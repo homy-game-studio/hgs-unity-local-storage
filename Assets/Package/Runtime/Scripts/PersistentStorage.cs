602491ee781b10af53ca0830a6cd37e8755c2f1b
Matheus Moura <matheus.moura13@gmail.com>
2022-07-21 01:33:52 -0300

    feat: adiciona PersistentStorage

---
+++ Assets/Package/Runtime/Scripts/PersistentStorage.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace HGS.FileControl
+{
+  [CreateAssetMenu(fileName = "PersistentStorage", menuName = "HGS/Storage/PersistentStorage")]
+  public class PersistentStorage : Storage { }
+}
